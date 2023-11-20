5f70bf60d7666b80070d59a26871d11f3debfe69
mjmayuga <mjm20mayuga@gmail.com>
2023-11-20 21:48:07 +0800

    testing lang

--- dapperAPMS/urls.py
+++ dapperAPMS/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for dapperAPMS project.
+URL configuration for dapperAPMS project. happylang
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
